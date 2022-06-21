2bce3874d21d6b1784d4f7eda368697e7d24d111
Emmanuel <soligboemmanuel@yahoo.com>
2022-06-21 21:49:18 +0100

    Modify the Documentation

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-The Project is being created by Emmanuel Soligbo for Shyft Power solution Interview Assignment
+# The Project is being created by Emmanuel Soligbo for Shyft Power solution Interview Assignment
 
 The Project is used to Get a sample user data and also use to get sample residence who uses shyft Power.
 
@@ -14,4 +14,18 @@ The App has a Main Page Home and 4 Components.
 
 The Components are Footer, Header, Location, Profile.
 
-The App has one service which is apiservice which helps to connect to our backend
+Header Component Contains the menu of the App and the Logo.
+
+Footer component contains the footer menu of the App.
+
+Profile Component is used to display the use details like name and Profile Information.
+
+Location Component is used to display the power consumption used by a residence.
+
+The App has one service which is apiservice which helps to connect to our backend.
+
+The api services has two api which are getusers and getresidence.
+
+I used and Observable to get data from the api and not promise.
+
+The app.routing modules has the homepage route which contains the major page of the App..
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,6 +7,8 @@ import { ApiservicesService } from 'src/app/services/apiservices.service';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+
+  //Use to get the reference of the location component
   @ViewChild('appcont1',{"static":false}) appcont1: any;
   residentid='';
 
@@ -16,28 +18,37 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  
+  //This is used to Start the search when you leave the search box
   startsearch($event:any){
+    //Check if the textbox is empty
     if($event.target.value ==""){
       this.residentid="";
       return;
     }
+    //Check if the text box contains anything apart from number
     if(isNaN($event.target.value )){
       this.residentid="";
       alert("Please Enter a valid Resisdent ID");
       return;
     }
+
+    //If the textbox content is valid set the value of resident ID in the api serivce and call send message to location services to load the current resident details
     this.residentid=$event.target.value;
     this.apiservice.residenceid = this.residentid;
     this.appcont1.getvalue();
   }
 
+
+  //Use start the Search when you click the button
   startsearchbtn()
   {
+     //Check if the textbox is empty
     if(this.residentid==''){
       alert("Enter A resident ID");
       return;
     }
+     //If the textbox content is valid set the value of resident ID in the api serivce and call send message to location services to load the current resident details
+   
     this.apiservice.residenceid =this.residentid;
     this.appcont1.getvalue();
   }
